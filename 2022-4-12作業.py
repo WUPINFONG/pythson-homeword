ec4fcc0fe1b0846bfad3f8ebc7b1331801e5e4e3
WUPINFONG <103197258+WUPINFONG@users.noreply.github.com>
2022-04-13 22:41:53 +0800

    Add files via upload

---
+++ 2022-4-12作業.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 22:38:44 2022
+
+@author: perry
+"""
+#作業1
+for i in range(1, 6):
+    for j in range(i):
+        print(i, end='')
+    print()
+
+#作業二
+for i in range(5,0,-1 ):
+    for j in range(i):
+        print(i, end='')
+    print()
+
+#作業3
+for i in range(9,0,-2 ):
+    for j in range(i):
+        print(i, end='')
+    print()
